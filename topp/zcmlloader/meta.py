8c0986eb3519da999031a092cbf55ea5711f41c4
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-01-30 16:51:29 +0000

    merge misnamed z3cautoinclude branch
    git-svn-id: https://svn.socialplanning.org/svn/ZCMLLoader/trunk@13032 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- topp/zcmlloader/meta.py
+++ topp/zcmlloader/meta.py
@@ -5,17 +5,18 @@ import pkg_resources as pkr
 from zope.interface import Interface
 from zope.schema import BytesLine
 from zope.schema import TextLine
+from zope.dottedname.resolve import resolve
 import os
 
 
-class IOpencoreConfigFile(Interface):
+class IConfigFile(Interface):
     """a config file to load up"""
     file = BytesLine(
         title=u'Configuration file name',
         description=u'Name of a configuration file to be loaded',
         required=True)
 
-class IOpencoreEntryPoints(Interface):
+class IEntryPoints(Interface):
     """an entry point group to load up"""
     group = TextLine(
         title=u'Entry point group name',
@@ -40,20 +41,19 @@ class IIncludeOverrides(Interface):
         )
 
 
-_opencore_config = None
+_config = None
 
-def load_opencore_config(_context, file=None):
-    global _opencore_config
-        
+def load_config(_context, file=None):
+    global _config
     if file is None:
         # do default?
         pass
     cp = ConfigParser()
     cp.read(file)
-    _opencore_config = cp
+    _config = cp
 
 def load_entry_points(_content, group=None):
-    global _opencore_config
+    global _config
 
     if group is None:
         # do default?
@@ -63,30 +63,31 @@ def load_entry_points(_content, group=None):
         dist = ep.dist
         dist_name = dist.project_name
         filename = ep.load()
-        _opencore_config.set(ep.name, dist_name, filename)
+        _config.set(ep.name, dist_name, filename)
 
 def load(_context, zcmlgroup='configure.zcml', override=False):
-    global _opencore_config
+    global _config
     include = xmlconfig.include
     if override:
         include = xmlconfig.includeOverrides
-    cp = _opencore_config
+    cp = _config
+
     try:
         items = cp.items(zcmlgroup)
     except NoSectionError:
         print "no section: %s" %zcmlgroup
         return
 
-    for dist, filename in items:
+    for dist, dotted_package in items:
         req = pkr.Requirement.parse(dist)
-        if not filename.endswith('/'):
-            filename = pkr.resource_filename(req, filename)
-        else:
-            filename = pkr.resource_filename(req, zcmlgroup)
-        include(_context, filename)
+        module_path = os.path.join(*dotted_package.split('.'))
+        filename = os.path.join(module_path, zcmlgroup)
+        filename = pkr.resource_filename(req, filename)
+        dep_package = resolve(dotted_package)
+        include(_context, file=filename, package=dep_package)
 
 def load_overrides(_context, zcmlgroup='overrides.zcml'):
     load(_context, zcmlgroup, override=True)
     
-def get_opencore_config(self):
-    return _opencore_config
+def get_config(self):
+    return _config
